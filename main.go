0d829526f6fe2d4436b3cf262f0c7a418ee22462
hao_bui <hao.bui@yedda.ai>
2021-07-11 23:13:01 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,18 @@
+
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love Minh Trang, Bao Tran!")
+}
+
+func main() {
+    http.HandleFunc("/", handler)
+		fmt.Println("Start http server at port 8080")
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
