e18ff98bd85571ca3155ab59a2f024347a073fe7
hao_bui <hao.bui@yedda.ai>
2021-07-12 11:10:51 +0700

    ngan phung

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Chao dang do, toi muon dit Minh Trang va Luong Nguyen!")
+    fmt.Fprintf(w, "Chao dang do, toi muon dit Minh Trang va Luong Nguyen ca Ngan Phung!")
 }
 
 func main() {
